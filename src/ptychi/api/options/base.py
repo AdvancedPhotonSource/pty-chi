56d58e1781cfa4e97f9946c46a0c1ccf00ea659d
Ming Du <ming.du101@gmail.com>
2025-06-20 12:11:56 -0500

    REFACTOR: make `OptimizationPlan` a subclass of `Options` and move it to `api.options.base`; fix tester field names

--- src/ptychi/api/options/base.py
+++ src/ptychi/api/options/base.py
@@ -13,7 +13,6 @@ import torch
 import numpy as np
 
 import ptychi.api.enums as enums
-from ptychi.api.options.plan import OptimizationPlan
 
 if TYPE_CHECKING:
     import ptychi.api.options.task as task_options
@@ -37,6 +36,46 @@ class Options:
         """Check if options values are valid.
         """
         return
+
+
+@dataclasses.dataclass
+class OptimizationPlan(Options):
+    """
+    When a `ReconstructParameter` has `optimizable == True`, this class is used to specify
+    the start, stop, and stride epochs of the optimization for that parameter. This class is
+    also used by `FeatureOptions`.
+    """
+    start: int = 0
+    """
+    The starting epoch.
+    """
+
+    stop: Optional[int] = None
+    """
+    The starting epoch. If None, optimization will run to the last epoch if the parameter
+    is optimizable.
+    """
+
+    stride: int = 1
+    """
+    The stride in epochs. Optimization will run every `stride` epochs.
+    """
+
+    def is_enabled(self, epoch: int) -> bool:
+        if self.start is not None and epoch < self.start:
+            return False
+        if self.stop is not None and epoch >= self.stop:
+            return False
+        if self.start is None:
+            return True
+        return (epoch - self.start) % self.stride == 0
+
+    def is_in_optimization_interval(self, epoch: int) -> bool:
+        if self.start is not None and epoch < self.start:
+            return False
+        if self.stop is not None and epoch >= self.stop:
+            return False
+        return True
 
 
 @dataclasses.dataclass
