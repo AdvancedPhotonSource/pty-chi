aa91c11b2c8fc5f7dbe25abc59b1e6b639254608
Ming Du <ming.du101@gmail.com>
2025-06-20 15:55:24 -0500

    CHORE: set default of _use_torch_compile to True

--- src/ptychi/utils.py
+++ src/ptychi/utils.py
@@ -20,7 +20,7 @@ if TYPE_CHECKING:
 
 
 _default_complex_dtype = torch.complex64
-_use_torch_compile = False
+_use_torch_compile = True
 
 
 def get_suggested_object_size(positions_px, probe_shape, extra=0):
