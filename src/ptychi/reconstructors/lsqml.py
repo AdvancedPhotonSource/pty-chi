f0ed9dd8b85a6e88eb062be597d9a2c5bcec2c51
Ashish Tripathi <atripath@atripathi.xray.aps.anl.gov>
2025-08-06 11:11:56 -0500

    lsqml correct step length

--- src/ptychi/reconstructors/lsqml.py
+++ src/ptychi/reconstructors/lsqml.py
@@ -637,6 +637,34 @@ class LSQMLReconstructor(AnalyticalIterativePtychographyReconstructor):
         alpha_vec = torch.linalg.solve(a_mat, b_vec)
         alpha_vec = alpha_vec.real.clip(0, None)
 
+
+        
+        
+        '''
+
+        # NOTE: FoldSlice does not use real() for off-diagonal terms, and it only computes this for the dominant mode (we use all incoherent modes here)
+        # sum over r (spatial pixel index), p (incoherent mode index)
+        a11 = torch.sum(delta_o_patches_p.abs() ** 2, dim=(-1, -2, -3)) + 1e-6
+        a11 += 0.5 * torch.mean(a11, dim=0)
+        a22 = torch.sum(delta_p_o.abs() ** 2, dim=(-1, -2, -3)) + 1e-6
+        a22 += 0.5 * torch.mean(a22, dim=0)
+        a12 = torch.sum(torch.real(delta_o_patches_p * delta_p_o.conj()), dim=(-1, -2, -3))
+        a21 = a12
+        b1 = torch.sum(torch.real(delta_o_patches_p.conj() * chi), dim=(-1, -2, -3))
+        b2 = torch.sum(torch.real(delta_p_o.conj() * chi), dim=(-1, -2, -3))
+
+        a_mat = torch.stack([a11, a12, a21, a22], dim=1).view(-1, 2, 2)
+        b_vec = torch.stack([b1, b2], dim=1).view(-1, 2).type(a_mat.dtype)
+        alpha_vec2 = torch.linalg.solve(a_mat, b_vec)
+
+        # alpha_vec = torch.where(alpha_vec < 0, 0, alpha_vec)
+
+        '''
+
+
+
+
+
         alpha_o_i = alpha_vec[:, 0]
         alpha_p_i = alpha_vec[:, 1]
 
