9715208682f58a877d904edb7ba9589e3518c0b1
yijiang1 <yj245@cornell.edu>
2025-09-25 00:02:23 -0500

    fix summary message in batch recon

--- src/ptychi/pear.py
+++ src/ptychi/pear.py
@@ -481,11 +481,12 @@ ptycho_recon(run_recon=True, **params)
         
         # Print summary of processing
         #print(f"Successfully processed scans: {successful_scans}")
-        print(f"Number of completed scans:{len(successful_scans)}/{end_scan - start_scan + 1}.")
+        print(f"Number of completed scans:{len(successful_scans)}/{len(scan_list)}.")
         print(f"Number of failed scans:{len(failed_scans)}.")
         print(f"Number of ongoing scans:{len(ongoing_scans)}.")
+        print(f"Number of excluded scans:{len(exclude_scans)}.")
         
-        if len(successful_scans) == end_scan - start_scan + 1:
+        if len(successful_scans) == len(scan_list) - len(exclude_scans):
             print(f"All scans completed successfully")
             break
         else:
