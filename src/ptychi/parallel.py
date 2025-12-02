FIX: sync indices when calculating averaged step sizes in `apply_reconstruction_parameter_updates`

src/ptychi/parallel.py
@@ -29,21 +29,30 @@ class MultiprocessMixin:
             return dist.get_world_size()
         except ValueError:
             return 1
-    
-    def get_chunk_of_current_rank(self, tensor: Tensor) -> Tensor:
+
+    def get_chunk_of_current_rank(
+        self,
+        tensor: Tensor,
+        return_chunk_sizes: bool = False,
+    ) -> Tensor:
         """
         Get a chunk of the tensor for the current rank.
-        
+
         Parameters
         ----------
         tensor : Tensor
             The tensor to be chunked. The first dimension of the tensor is assumed
             to be the batch dimension and it will be split along this dimension.
-            
+        return_chunk_sizes : bool
+            If True, return a list of integers representing the chunk sizes for each rank.
+
         Returns
         -------
         Tensor
             A chunk of the tensor for the current rank.
+        list[int]
+            A list of integers representing the chunk sizes for each rank, Returned
+            only if return_chunk_sizes is True.
         """
         chunks = torch.chunk(tensor, self.n_ranks, dim=0)
         if len(chunks) != self.n_ranks:
@@ -51,15 +60,26 @@ class MultiprocessMixin:
             chunk_size = tensor.shape[0] // self.n_ranks
             start = self.rank * chunk_size
             end = min(start + chunk_size, tensor.shape[0])
-            return tensor[start:end]
+            chunk = tensor[start:end]
+            if return_chunk_sizes:
+                chunk_sizes = [
+                    min(r * chunk_size + chunk_size, tensor.shape[0]) - r * chunk_size
+                    for r in range(self.n_ranks)
+                ]
         else:
-            return chunks[self.rank]
+            chunk = chunks[self.rank]
+            if return_chunk_sizes:
+                chunk_sizes = [len(c) for c in chunks]
+        if return_chunk_sizes:
+            return chunk, chunk_sizes
+        return chunk
 
     def sync_buffer(
-        self, buffer: Any, 
-        indices: Optional[Tensor] = None, 
+        self,
+        buffer: Any,
+        indices: Optional[Tensor] = None,
         source_rank: Optional[int] = None,
-        op: dist.ReduceOp = dist.ReduceOp.SUM
+        op: dist.ReduceOp = dist.ReduceOp.SUM,
     ) -> Any:
         """Synchronize a buffer across ranks.
         

src/ptychi/reconstructors/lsqml.py
@@ -1271,10 +1271,16 @@ class LSQMLReconstructor(AnalyticalIterativePtychographyReconstructor):
 
 
 class MultiprocessLSQMLReconstructor(LSQMLReconstructor, MultiprocessMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.chunk_sizes_all_ranks = None
+
     def run_minibatch(self, input_data, y_true, *args, **kwargs) -> None:
         indices = input_data[0]
         full_indices = indices.clone()
-        indices = self.get_chunk_of_current_rank(full_indices)
+        indices, self.chunk_sizes_all_ranks = self.get_chunk_of_current_rank(
+            full_indices, return_chunk_sizes=True
+        )
         if len(indices) == 0:
             raise ValueError(
                 "A rank didn't get any data in this minibatch. This happens when "
@@ -1394,6 +1400,29 @@ class MultiprocessLSQMLReconstructor(LSQMLReconstructor, MultiprocessMixin):
         delta_p_hat = delta_p_hat / bsize_all_ranks
         return delta_p_hat
 
+    def apply_reconstruction_parameter_updates(self, indices: torch.Tensor):
+        """Perform updates for reconstruction parameters using the
+        update vectors stored in the `grad` attribute of the corresponding
+        `ReconstructionParameter` objects.
+
+        This method overrides the parent method to synchronize buffer across ranks.
+
+        Parameters
+        ----------
+        indices : torch.Tensor
+            The indices of the diffraction patterns processed in the current batch.
+        """
+        tensor_list = [
+            torch.zeros(
+                self.chunk_sizes_all_ranks[r],
+                dtype=indices.dtype,
+                device=torch.get_default_device(),
+            )
+            for r in range(self.n_ranks)
+        ]
+        dist.all_gather(tensor_list, indices.to(torch.get_default_device()))
+        indices_all_ranks = torch.cat(tensor_list)
+        super().apply_reconstruction_parameter_updates(indices_all_ranks)
 
     def run_post_epoch_hooks(self) -> None:
         if self.current_epoch == 0 and self.options.rescale_probe_intensity_in_first_epoch: