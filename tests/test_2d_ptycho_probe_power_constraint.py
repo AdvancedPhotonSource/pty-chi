80b2cb30fff0aac4ce1ca150e2049c26becd4ec1
Ming Du <ming.du101@gmail.com>
2026-02-04 16:59:44 -0600

    TEST: change test_2d_ptycho_probe_power_constraint.py to unit tester

--- tests/test_2d_ptycho_probe_power_constraint.py
+++ tests/test_2d_ptycho_probe_power_constraint.py
@@ -1,186 +1,52 @@
-import argparse
-import logging
-
 import torch
-import numpy as np
 
-import ptychi.api as api
-from ptychi.api.task import PtychographyTask
-from ptychi.api import LSQMLOptions, AutodiffPtychographyOptions
-import ptychi.utils as utils
+from ptychi.api.options import base as obase
+from ptychi.data_structures.object import Object
+from ptychi.data_structures.probe import Probe
 
-import test_utils as tutils
 
-class Tester2DPtychoProbePowerConstraint(tutils.TungstenDataTester):
+def _make_object(data: torch.Tensor) -> Object:
+    options = obase.ObjectOptions()
+    options.optimizable = False
+    return Object(data=data, options=options)
 
-    @tutils.TungstenDataTester.wrap_recon_tester(name='test_2d_ptycho_probe_power_constraint_lsqml_no_propagation')
-    def test_2d_ptycho_probe_power_constraint_lsqml_no_propagation(self):
-        self.setup_ptychi(cpu_only=False)
 
-        data, probe, pixel_size_m, positions_px = self.load_tungsten_data(pos_type='true', additional_opr_modes=3)
-        object_init = torch.ones(
-            [1, *utils.get_suggested_object_size(positions_px, probe.shape[-2:], extra=100)], 
-            dtype=utils.get_default_complex_dtype()
-        )
-        
-        options = LSQMLOptions()
-        options.data_options.data = data
-        
-        options.object_options.initial_guess = object_init
-        options.object_options.pixel_size_m = pixel_size_m
-        options.object_options.optimizable = True
-        options.object_options.optimizer = api.Optimizers.SGD
-        options.object_options.step_size = 1
-        
-        options.probe_options.power_constraint.probe_power = np.mean(np.sum(data, (-1, -2)), axis=0) / np.prod(data.shape[-2:]) 
-        options.probe_options.power_constraint.enabled = True
-        
-        options.probe_options.initial_guess = probe * torch.sqrt(options.probe_options.power_constraint.probe_power / torch.sum(torch.abs(probe[0,...])**2)) 
-        options.probe_options.optimizable = True
-        options.probe_options.optimizer = 'sgd'
-        options.probe_options.step_size = 1
-        
-        options.probe_position_options.position_x_px = positions_px[:, 1]
-        options.probe_position_options.position_y_px = positions_px[:, 0]
-        options.probe_position_options.correction_options.update_magnitude_limit = 5.0
-        options.probe_position_options.optimizable = True
-        options.probe_position_options.optimizer = api.Optimizers.ADAM
-        options.probe_position_options.step_size = 1e-1
-        
-        options.opr_mode_weight_options.initial_weights = np.array([1, 1e-6, 1e-6, 1e-6])
-        options.opr_mode_weight_options.optimize_intensity_variation = True
-        options.opr_mode_weight_options.optimizable = True
-        
-        options.reconstructor_options.num_epochs = 4
-        options.reconstructor_options.batch_size = 40
-        options.reconstructor_options.default_device = api.Devices.GPU
-        options.reconstructor_options.displayed_loss_function = api.LossFunctions.MSE_SQRT
-        options.reconstructor_options.allow_nondeterministic_algorithms = False
-        
-        with PtychographyTask(options) as task:
-            task.run()
-            # This should be equivalent to:
-            # for _ in range(64):
-            #     task.run(1)
-            
-            recon = task.get_data_to_cpu(name='object', as_numpy=True)[0]
-        return recon
-    
-    @tutils.TungstenDataTester.wrap_recon_tester(name='test_2d_ptycho_probe_power_constraint_lsqml')
-    def test_2d_ptycho_probe_power_constraint_lsqml(self):
-        self.setup_ptychi(cpu_only=False)
+def _make_probe(data: torch.Tensor, probe_power: float, scale_object: bool = True) -> Probe:
+    options = obase.ProbeOptions()
+    options.optimizable = False
+    options.power_constraint.probe_power = probe_power
+    options.power_constraint.scale_object = scale_object
+    return Probe(data=data, options=options)
 
-        data, probe, pixel_size_m, positions_px = self.load_tungsten_data(pos_type='true', additional_opr_modes=3)
-        object_init = torch.ones(
-            [1, *utils.get_suggested_object_size(positions_px, probe.shape[-2:], extra=100)], 
-            dtype=utils.get_default_complex_dtype()
-        )
-        
-        options = LSQMLOptions()
-        options.data_options.data = data
-        
-        options.object_options.initial_guess = object_init
-        options.object_options.pixel_size_m = pixel_size_m
-        options.object_options.optimizable = True
-        options.object_options.optimizer = api.Optimizers.SGD
-        options.object_options.step_size = 1
-        
-        options.probe_options.initial_guess = probe
-        options.probe_options.optimizable = True
-        options.probe_options.optimizer = 'sgd'
-        options.probe_options.step_size = 1
-        options.probe_options.power_constraint.probe_power = data[0].sum()
-        options.probe_options.power_constraint.enabled = True
-        
-        options.probe_position_options.position_x_px = positions_px[:, 1]
-        options.probe_position_options.position_y_px = positions_px[:, 0]
-        options.probe_position_options.correction_options.update_magnitude_limit = 5.0
-        options.probe_position_options.optimizable = True
-        options.probe_position_options.optimizer = api.Optimizers.ADAM
-        options.probe_position_options.step_size = 1e-1
-        
-        options.opr_mode_weight_options.initial_weights = np.array([1, 1e-6, 1e-6, 1e-6])
-        options.opr_mode_weight_options.optimize_intensity_variation = True
-        options.opr_mode_weight_options.optimizable = True
-        
-        options.reconstructor_options.num_epochs = 4
-        options.reconstructor_options.batch_size = 40
-        options.reconstructor_options.default_device = api.Devices.GPU
-        options.reconstructor_options.displayed_loss_function = api.LossFunctions.MSE_SQRT
-        options.reconstructor_options.allow_nondeterministic_algorithms = False
-        
-        with PtychographyTask(options) as task:
-            task.run()
-            # This should be equivalent to:
-            # for _ in range(64):
-            #     task.run(1)
-            
-            recon = task.get_data_to_cpu(name='object', as_numpy=True)[0]
-        return recon
-        
-    @tutils.BaseTester.wrap_recon_tester(name='test_2d_ptycho_probe_power_constraint_ad')
-    def test_2d_ptycho_probe_power_constraint_ad(self):
-        self.setup_ptychi(cpu_only=False)
 
-        data, probe, pixel_size_m, positions_px = self.load_tungsten_data(pos_type='true', additional_opr_modes=3)
-        object_init = torch.ones(
-            [1, *utils.get_suggested_object_size(positions_px, probe.shape[-2:], extra=100)], 
-            dtype=utils.get_default_complex_dtype()
-        )
-        
-        options = AutodiffPtychographyOptions()
-        options.data_options.data = data
-        
-        options.object_options.initial_guess = object_init
-        options.object_options.pixel_size_m = pixel_size_m
-        options.object_options.optimizable = True
-        options.object_options.optimizer = api.Optimizers.SGD
-        options.object_options.step_size = 1e-1
-        
-        options.probe_options.initial_guess = probe
-        options.probe_options.optimizable = True
-        options.probe_options.optimizer = api.Optimizers.SGD
-        options.probe_options.step_size = 1e-1
-        options.probe_options.power_constraint.probe_power = data[0].sum()
-        options.probe_options.power_constraint.enabled = True
-        
-        options.probe_position_options.position_x_px = positions_px[:, 1]
-        options.probe_position_options.position_y_px = positions_px[:, 0]
-        options.probe_position_options.optimizable = True
-        options.probe_position_options.optimizer = api.Optimizers.SGD
-        options.probe_position_options.step_size = 1e-1
-        
-        options.opr_mode_weight_options.initial_weights = np.array([1, 1e-6, 1e-6, 1e-6])
-        options.opr_mode_weight_options.optimize_intensity_variation = True
-        options.opr_mode_weight_options.optimizable = True
-        options.opr_mode_weight_options.optimizer = api.Optimizers.SGD
-        options.opr_mode_weight_options.step_size = 1e-3
-        
-        options.reconstructor_options.num_epochs = 4
-        options.reconstructor_options.batch_size = 40
-        options.reconstructor_options.default_device = api.Devices.GPU
-        options.reconstructor_options.displayed_loss_function = api.LossFunctions.MSE_SQRT
-        options.reconstructor_options.allow_nondeterministic_algorithms = False
-        
-        with PtychographyTask(options) as task:
-            task.run()
-            # This should be equivalent to:
-            # for _ in range(64):
-            #     task.run(1)
-            
-            recon = task.get_data_to_cpu(name='object', as_numpy=True)[0]
-            
-        return recon
-    
-    
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--generate-gold', action='store_true')
-    args = parser.parse_args()
+def test_probe_power_constraint_scales_probe_to_target():
+    probe_data = torch.ones((1, 1, 2, 2), dtype=torch.complex64)
+    object_data = torch.full((1, 2, 2), 2 + 0j, dtype=torch.complex64)
 
-    tester = Tester2DPtychoProbePowerConstraint()
-    tester.setup_method(name="", generate_data=False, generate_gold=args.generate_gold, debug=True)
-    tester.test_2d_ptycho_probe_power_constraint_lsqml_no_propagation()
-    tester.test_2d_ptycho_probe_power_constraint_ad()
-    tester.test_2d_ptycho_probe_power_constraint_lsqml()
-    
+    target_power = 8.0
+    probe = _make_probe(probe_data, probe_power=target_power, scale_object=True)
+    obj = _make_object(object_data)
+
+    probe.constrain_probe_power(obj)
+
+    probe_power = torch.sum(torch.abs(probe.get_opr_mode(0)) ** 2)
+    assert torch.isclose(probe_power, torch.tensor(target_power, dtype=probe_power.dtype))
+
+    scale = torch.sqrt(torch.tensor(target_power, dtype=probe_power.dtype) / 4.0)
+    expected_object = object_data / scale
+    assert torch.allclose(obj.data, expected_object)
+
+
+def test_probe_power_constraint_respects_scale_object_option():
+    probe_data = torch.ones((1, 1, 2, 2), dtype=torch.complex64)
+    object_data = torch.full((1, 2, 2), 3 + 0j, dtype=torch.complex64)
+
+    target_power = 4.0
+    probe = _make_probe(probe_data, probe_power=target_power, scale_object=False)
+    obj = _make_object(object_data)
+
+    probe.constrain_probe_power(obj)
+
+    probe_power = torch.sum(torch.abs(probe.get_opr_mode(0)) ** 2)
+    assert torch.isclose(probe_power, torch.tensor(target_power, dtype=probe_power.dtype))
+    assert torch.allclose(obj.data, object_data)
